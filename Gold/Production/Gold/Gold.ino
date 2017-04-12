fd0fc2a075150acc46ee196b0956a7ed9c82dd5c
June <spd1335@outlook.com>
2017-04-12 22:00:42 +0900

    Gold Init

--- Gold/Production/Gold/Gold.ino
+++ Gold/Production/Gold/Gold.ino
@@ -1,35 +1,62 @@
-/*
-* Bluetooh Basic: LED ON OFF - Avishkar
-* Coder - Mayoogh Girish
-* Website - http://bit.do/Avishkar
-* Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
-* This program lets you to control a LED on pin 13 of arduino using a bluetooth module
-*/
+#include <SoftwareSerial.h>
+#include "HX711.h"
+
+#define DATA A0
+#define CLK A1
+HX711 _scale;
+SoftwareSerial BTserial(2, 3); // RX | TX
+
+char c = ' ';
+
 char data = 0;            //Variable for storing received data
 char data2 = '\0';
 void setup()
 {
-    Serial.begin(9600);   //Sets the baud for serial data transmission 
+    Serial.begin(115200);
+    BTserial.begin(115200);
+    _scale.begin(DATA, CLK);
+    
+    _scale.set_scale(146.95);
+    _scale.tare();
+    _scale.power_down();
 }
 void loop()
 {
-   if(Serial.available() > 0)      // Send data only when you receive data:
-   {
-      data = Serial.read();        //Read the incoming data & store into data
-      data2 = data;
-      Serial.print(data);          //Print Value inside data in Serial monitor
-            
-   }
-   else
-   {
-    if(data2 != '\0')
-    {
-      Serial.print("\n");
-      Serial.print("Rcv Complete.");
-      Serial.print("\n");
-      data2 = '\0';
-    }
-    
-   }
-   delay(10);
+  int weight = 0;
+  if (BTserial.available())
+  {  
+      c = BTserial.read();
+
+      switch(c)
+      {
+        case 'M':
+        case 'm':
+          _scale.power_up();
+          weight = ((int)((_scale.get_units(10) / 10.0) + 0.5) * 10);
+          BTserial.println(weight);
+          break;
+         case 'T':
+         case 't':
+          _scale.power_up();
+          _scale.tare();
+          break;           
+      }
+      _scale.power_down();
+  }
+  
+  if (Serial.available())
+  {
+      c =  Serial.read();
+      BTserial.write(c);
+  }
+  
+  delay(100);
 }
+
+void reset(HX711 scale, int delayMS)
+{
+  scale.power_down();              // put the ADC in sleep mode
+  delay(delayMS);
+  scale.power_up();
+}
+
